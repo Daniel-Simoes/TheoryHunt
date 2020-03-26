675e98fc982044d4292f41e62b8e70d8dc456b93
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-26 21:46:04 +0000

    Fix Routes by Express(Server)

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+ 
+ // Express to Criate the Server
  const express = require('express');
-
  const server =  express()
 
- server.get('/', function() {
-    console.log('Server Working...')
+//Fix statics files (CSS, SCRIPTS and IMAGENS)
+server.use(express.static('public'))
+
+
+// The Router to get the HTML
+ server.get('/', function(req, res) {
+    return res.sendFile(__dirname + '/index.html')
  })
 
+ server.get('/theorys', function(req, res) {
+    return res.sendFile(__dirname + '/theorys.html')
+ })
+
+
+ // Server Door
  server.listen(3000)
