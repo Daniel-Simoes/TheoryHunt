8693b7c21495c6dcffb131d797bb99b292e0d196
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-27 14:06:06 +0000

    Form Validation

--- public/scripts.js
+++ public/scripts.js
@@ -15,3 +15,28 @@ function onOff() {
         .toggle("addScroll")
 }
  
+function checkFields(event) {
+
+    const valuesToCheck = [
+        "title",
+        "image",
+        "category",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = valuesToCheck.fild(function(value) {
+
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIsEmpty = !event.target[value].value.trim()
+        
+        if(checkIfIsString && checkIsEmpty) {
+            return true
+        }
+    })
+    if(isEmpty) {
+        event.preventDefault()
+        alert("Please, put all information about your Theory.")
+    }
+
+}
